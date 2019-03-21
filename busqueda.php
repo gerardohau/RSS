d402049c8d95983055297323fcd4db2ae13c7ed2
GerardoHau <gerardohau97@gmail.com>
2019-03-21 10:48:11 -0600

    Resultados busqueda arreglados

--- busqueda.php
+++ busqueda.php
@@ -23,6 +23,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
         echo $html;
     } else{
+
+        //Arreglar el boton de crear Excel aparece hasta arriba
         $_POST['indexar'] = false;
         include('index.php');     
         if(!(strlen($palabrabuscar)>0)){
@@ -33,14 +35,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }else{
 
             echo "<div class='results'>";
-            if ($datos->num_rows > 0) {
+            if ($datos->num_rows > 0) { 
+                echo "<div class='container'> <section class='col-xs-12 col-sm-6 col-md-12'>
+                <article class='search-result row'>";
                 while ($user = mysqli_fetch_array($datos)) {
 
-                    $titulo = $user['titulo'];
+                    $titulo = " " . $user['titulo'];
                     $link = $user['link'];
                     $ide = $user['id'];
-                    echo "<input type='checkbox' class='seleccionados' value='$ide'><a class=\"links\" href='$link'>$titulo</a>" . "<br>";
+                    $descripcion=$user['descripcion'] . "..";
+                    $fecha=$user['fecha'];
+        
+                    echo "<div class='col-xs-12 col-sm-12 col-md-3'>
+                    <a href='#' title='Lorem ipsum' class='thumbnail'><img src='http://lorempixel.com/250/140/people' alt='Lorem ipsum' /></a></div>
+                    <div class='col-xs-12 col-sm-12 col-md-2'>
+                    <ul class='meta-search'>
+                    <li><i class='glyphicon glyphicon-calendar'></i> <span>$fecha</span></li>
+                    </ul>
+                    </div> 
+                    <div class='col-xs-12 col-sm-12 col-md-7 excerpet'>
+				    <h3><a href='$link' title=''>$titulo</a></h3>
+				    <p text-overflow: ellipsis;>$descripcion </p>						
+                    <span class='plus'><input type='checkbox' class='seleccionados' value='$ide'></a></span>
+                    </div>
+                    <span class='clearfix borda'></span>";
                 }
+                echo "</section></div>";
             } else {
                 echo"<span id='error2'>No se encontró ninguna coincidencia</span><br>";
             }
